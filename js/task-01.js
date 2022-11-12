efe374343ee59b146c545e8bff0fe041beec8531
VladislavVolyanskiy <vladislavvolyanskiy@gmail.com>
2022-11-12 21:41:48 +0200

    tasks 1-10

--- js/task-01.js
+++ js/task-01.js
@@ -2,9 +2,6 @@ const allCatItems = document.querySelectorAll('.item');
 
 console.log('Number of categories:', allCatItems.length);
 
-// const allCatItems = document.querySelectorAll('#categories > .item');
-// console.log(`Number of categories: ${allCategories.length}`);
-
 allCatItems.forEach(itemEl => {
 	console.log('Category: ', itemEl.querySelector('h2').textContent);
 	console.log('Elements: ', itemEl.querySelectorAll('li').length);
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,36 @@
+const refs = {
+	input: document.querySelector('input'),
+	btnCreate: document.querySelector('[data-create]'),
+	btnDestroy: document.querySelector('[data-destroy]'),
+	wrapForBoxes: document.querySelector('#boxes'),
+};
+
+refs.btnCreate.addEventListener('click', createBoxes);
+refs.btnDestroy.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+	const boxesArr = [];
+
+	amount = refs.input.value;
+
+	for (let i = 0; i < amount; i++) {
+		const box = document.createElement('div');
+
+		box.style.width = `${20 + i * 10}px`;
+		box.style.height = `${20 + i * 10}px`;
+		box.style.backgroundColor = getRandomHexColor();
+
+		boxesArr.push(box);
+	}
+	// console.log(boxesArr);
+	refs.wrapForBoxes.append(...boxesArr);
+}
+
+function destroyBoxes() {
+	refs.wrapForBoxes.innerHTML = '';
+	refs.input.value = '';
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
